143b6b09b6c7525486681f02d040aae86478ddb4
Jesus Lencina <jesusignaciolencina@gmail.com>
2020-11-04 21:53:35 -0300

    Commit 1

---
+++ src/assets/globalStyles.js
@@ -0,0 +1,7 @@
+const variables = {
+    mainColor: '#00D1B2',
+    secondaryColor: '#01D0B1',
+    gray: '#F4F4F4'
+}
+
+export default variables;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import variables from "../assets/globalStyles";
+
+const HeaderElement = styled.header`
+  background-color: ${variables.mainColor};
+  color: white;
+  padding: 3rem 4rem;
+  h1{
+      font-weight: 500;
+      margin-bottom: 0.25rem;
+  }
+
+  span {
+    font-weight: 650;
+  }
+`;
+
+function Header() {
+  return (
+    <HeaderElement>
+      <h1>Hoteles</h1>
+      <p>
+        Desde el <span>hardcodeado</span> hasta el <span>harcodeado</span>
+      </p>
+    </HeaderElement>
+  );
+}
+
+export default Header;
